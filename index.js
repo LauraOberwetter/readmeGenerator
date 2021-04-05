0d612168afdbc8ec8ff6287c4ffc62fc4fbee8f6
Laura Oberwetter <leoberwetter@gmail.com>
2021-04-04 21:50:21 -0500

    started return functions

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // TODO: Include packages needed for this application
 //"import inquirer"
 const inquirer = require("inquirer")
+const fs = require("fs")
+const path = require("path")//require a path package for the md file name
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -29,10 +31,12 @@ const questions = [
         message: 'How will this project be used?',
     },
     {
-        //drop down
-        type: 'input',
+        type: 'checkbox',
         name: 'License',
         message: 'What licensing does this project have?',
+        choices: [
+            'None','Apache', 'BSD', 'Creative Commons', 'Eclipse', 'GNU', 'IBM', 'MIT', 'Mozilla', 'Zlib'
+        ],
     },
     {
         type: 'input',
@@ -57,13 +61,22 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+     let testMarkdown = "#HEllo"
+     return fs.writeFileSync(path.join(process.cwd(), fileName), data) //file in current working directory
+
+}
 
 // TODO: Create a function to initialize app
 //"main" function, initializes application
 function init() {
-    inquirer.prompt(questions).then((responses) => {
-        console.log(responses)
+    inquirer.prompt(questions).then((responses) => { 
+        console.log(responses);
+        //key=name, value=answer
+        // call generate markdown?
+        writeToFile("README.md", data) //data=param representing passed content // potentially have user enter different name for md file
+        console.log(responses) 
+    // include responses.name to access answer responses?
     })
 }
 
